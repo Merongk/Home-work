8fee3ed8a3542b76623ad2ec8e89eaaea5bb6c66
Meron Ghebreyesus Kifleyesus <158231367+Merongk@users.noreply.github.com>
2024-07-06 00:09:42 +0200

    Your commit message here

--- addActivity.js
+++ addActivity.js
@@ -1,15 +1,48 @@
-export const activities = [];
+// const productivityScores = {
+//     'Work': 10,
+//     'Exercise': 8,
+//     'Leisure': 5,
+//     'Learning': 9,
+//     'Other': 4
+// };
 
-const productivityScores = {
-    'Work': 10,
-    'Exercise': 8,
-    'Leisure': 5,
-    'Learning': 9,
-    'Other': 4
-};
-
-export function addActivity(name, category, duration) {
-    const date = new Date().toISOString().split('T')[0];
-    const score = productivityScores[category] ? productivityScores[category] * duration : 0;
-    activities.push({ name, category, duration, date, score });
+const currentActivityies = [{
+    date: '23/7-18',
+    activity: 'Youtube',
+    duration: 130,
+},
+{
+    date: '23/7-18',
+    activity: 'Reading',
+    duration: 10,
+},
+{
+    date: '23/7-18',
+    activity: 'Swimming',
+    duration: 50,
+},
+{
+    date: '23/7-18',
+    activity: 'dancing',
+    duration: 35,
 }
+]
+
+// function addActivity(name, category, duration) {
+//     const date = new Date().toISOString().split('T')[0];
+//     const score = productivityScores[category] ? productivityScores[category] * duration : 0;
+//     activities.push({ name, category, duration, date, score });
+// }
+
+function findLongestActivity(activities) {
+    let maxDurationActivity = 0;
+
+    for (let index = 0; index < activities.length; index++) {
+        if (activities[index].duration > maxDurationActivity) {
+            maxDurationActivity = activities[index].duration
+        }        
+    }
+    return maxDurationActivity;
+}
+
+console.log(findLongestActivity(currentActivityies));
